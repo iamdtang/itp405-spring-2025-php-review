4b01032d073b227c11b30ab8c3c4a9af9efab39a
David Tang <687449+iamdtang@users.noreply.github.com>
2025-01-26 23:31:42 -0800

    Query invoices table and display records

---
+++ invoices.php
@@ -0,0 +1,54 @@
+<?php
+
+$pdo = new PDO('sqlite:chinook.db');
+
+$sql = '
+  SELECT invoices.InvoiceId, invoices.InvoiceDate, invoices.Total
+  FROM invoices
+  ORDER BY invoices.InvoiceDate DESC
+';
+
+$statement = $pdo->prepare($sql); // prepared statement
+$statement->execute();
+
+$invoices = $statement->fetchAll(PDO::FETCH_OBJ);
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Invoices</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+  <div class="container mt-3">
+    <table class="table table-striped">
+      <thead>
+        <tr>
+          <th>Date</th>
+          <th>Invoice Number</th>
+          <th>Total</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php foreach ($invoices as $invoice) : ?>
+          <tr>
+            <td>
+              <?php echo $invoice->InvoiceDate ?>
+            </td>
+            <td>
+              <?php echo $invoice->InvoiceId ?>
+            </td>
+            <td>
+              $<?php echo $invoice->Total ?>
+            </td>
+          </tr>
+        <?php endforeach ?>
+      </tbody>
+    </table>
+  </div>
+</body>
+</html>
+
